7cffde22b78adfcd0cceea134266b30cf1538cf3
Jasmin Trotter <jasmin.trotter@nike.com>
2020-10-27 13:58:02 -0700

    finished our cocoa framework

---
+++ cocoapod-tutorial/cocoapod-tutorial/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  cocoapod-tutorial
+//
+//  Created by Jasmin Trotter on 10/27/20.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "something was done"
+    }
+}
---
+++ cocoapod-tutorial/cocoapod-tutorial/cocoapod_tutorial.h
@@ -0,0 +1,18 @@
+//
+//  cocoapod_tutorial.h
+//  cocoapod-tutorial
+//
+//  Created by Jasmin Trotter on 10/27/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for cocoapod_tutorial.
+FOUNDATION_EXPORT double cocoapod_tutorialVersionNumber;
+
+//! Project version string for cocoapod_tutorial.
+FOUNDATION_EXPORT const unsigned char cocoapod_tutorialVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cocoapod_tutorial/PublicHeader.h>
+
+
